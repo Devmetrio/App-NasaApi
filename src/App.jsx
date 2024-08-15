12d5ef9fcb768366621edc99457d407be93017c9
German <briph21@gmail.com>
2024-08-14 23:49:53 -0500

    Inicio de app-NASA

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import SideBar from "./components/SideBar";
+
+function App() {
+  return (
+    <>
+        <SideBar></SideBar>
+        <Main></Main>
+        <Footer></Footer>
+    </>
+  )
+}
+
+export default App
